6c8f7379150bc26f551ff5d56970ba13cedeb6dc
Steve G <s.r.grace@gmail.com>
2013-03-11 22:30:39 -0600

    Updated IP
    Updated the IP. It now is <5ns! Still WIP.

---
+++ hls/ip/xgui/sha256_top_v1_00_a.tcl
@@ -0,0 +1,6 @@
+#Definitional proc to organize widgets for parameters.
+proc create_gui { ipview } {
+	set Page0 [ ipgui::add_page $ipview  -name "Page 0" -layout vertical]
+	set Component_Name [ ipgui::add_param  $ipview  -parent  $Page0  -name Component_Name ]
+}
+
