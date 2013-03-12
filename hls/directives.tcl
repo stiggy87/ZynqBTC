ee2c33dcccfe72183859f2107ff738280c141f1f
Steve G <s.r.grace@gmail.com>
2013-03-11 23:01:43 -0600

    Updated directives/scripts

--- hls/directives.tcl
+++ hls/directives.tcl
@@ -1,5 +1,19 @@
 ############################################################
 ## This file is generated automatically by Vivado HLS.
 ## Please DO NOT edit it.
-## Copyright (C) 2012 Xilinx Inc. All rights reserved.
+## Copyright (C) 2013 Xilinx Inc. All rights reserved.
 ############################################################
+#set_directive_interface -mode ap_memory sha256_top data
+#set_directive_interface -mode ap_memory sha256_top hash
+set_directive_interface -mode ap_fifo -depth 64 sha256_top data 
+set_directive_interface -mode ap_vld -register sha256_top hash
+set_directive_interface -mode ap_ctrl_none sha256_top
+#set_directive_interface -mode ap_memory sha256_top ctx.in_data
+#set_directive_interface -mode ap_bus -register sha256_top ctx.datalen
+#set_directive_interface -mode ap_bus -register sha256_top ctx.bitlen
+#set_directive_interface -mode ap_memory sha256_top ctx.state
+
+#set_directive_resource -core RAM_2P_BRAM sha256_top ctx.in_data
+#set_directive_resource -core RAM_2P_BRAM sha256_top ctx.state
+set_directive_resource -core RAM_2P_BRAM sha256_top hash
+set_directive_resource -core RAM_2P_BRAM sha256_top data
--- hls/script.tcl
+++ hls/script.tcl
@@ -1,1 +1,15 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 2013 Xilinx Inc. All rights reserved.
+############################################################
+open_project zynqBTC
+set_top sha256_top
+add_files sha256.c
+add_files -tb sha256_tb.c
+open_solution "solution2"
+set_part  {xc7z020clg484-1}
+create_clock -period 5
 
+source "./zynqBTC/solution2/directives.tcl"
+csynth_design
